afd540a5e6d56b8ae434d321a6dcac7e49579edf
SFMack <sfcamack@icloud.com>
2020-06-13 20:45:25 -0700

    session information is being stored in the database

--- api/server.js
+++ api/server.js
@@ -4,18 +4,27 @@ const usersRouter = require('../users/users-router.js');
 const server = express();
 const session = require('express-session');
 const cors = require('cors');
+const knexSessionStore = require('connect-session-knex')(session);
 
 const sessionConfig = {
   name: 'macksession', // default sid
   secret: 'super secret', // good place for environment variables
   cookie: {
-      maxAge: 1000 * 30, // 30 seconds
+      maxAge: 1000 * 60 * 60, // 30 seconds
       secure: false, // true in production
       httpOnly: true, // always set to true. dont allow javascript access   
   },
   resave: false, // recreate a session when no changes have happened
   saveUninitialized: false, // GDPR laws against setting cookies automatically. true after user has opted in.
+  store: new knexSessionStore({
+    knex: require('../data/dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true, // create table if it doesn't already exist
+    clearInterval: 1000 * 60 * 60
+  })
 }
+
 
 server.use(session(sessionConfig));
 server.use(express.json());
